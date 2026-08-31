31e2134aef03ac192dc34d694c02244b3b305fe9
Guilherme Savio <124003285+LadonGx@users.noreply.github.com>
2026-08-31 17:52:50 -0300

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        const isAlphanumeric = (char) => /[a-zA-Z0-9]/.test(char);
+        let left = 0;
+        let right = s.length - 1;
+        while (left < right) {
+            if (!isAlphanumeric(s[left])) {
+                left++;
+                continue;
+            }
+            if (!isAlphanumeric(s[right])) {
+                right--;
+                continue;
+            }
+
+            if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
